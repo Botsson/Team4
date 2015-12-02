c6895b2cb6b485beab598e2508ff96642594ed0b
Jimi Tähtinen <Jimi.Taehtinen@betssongroup.com>
2015-12-02 14:44:34 +0100

    update

--- Team4.ino
+++ Team4.ino
@@ -1,55 +1,21 @@
-/*
- * Demo line-following code for the Pololu Zumo Robot
- *
- * This code will follow a black line on a white background, using a
- * PID-based algorithm.  It works decently on courses with smooth, 6"
- * radius curves and has been tested with Zumos using 30:1 HP and
- * 75:1 HP motors.  Modifications might be required for it to work
- * well on different courses or with different motors.
- *
- * http://www.pololu.com/catalog/product/2506
- * http://www.pololu.com
- * http://forum.pololu.com
- */
-
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
 #include <ZumoMotors.h>
-#include <ZumoBuzzer.h>
 #include <Pushbutton.h>
 
-
-ZumoBuzzer buzzer;
 ZumoReflectanceSensorArray reflectanceSensors;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 int lastError = 0;
 
+int numberOfErrors = 0;
 // This is the maximum speed the motors will be allowed to turn.
 // (400 lets the motors go at top speed; decrease to impose a speed limit)
 const int MAX_SPEED = 200;
 
-#include <avr/pgmspace.h>  // this lets us refer to data in program space (i.e. flash)
-// store this fugue in program space using the PROGMEM macro.  
-// Later we will play it directly from program space, bypassing the need to load it 
-// all into RAM first.
-const char fugue[] PROGMEM = 
-  "! O5 L16 agafaea dac+adaea fa<aa<bac#a dac#adaea f"
-  "O6 dcd<b-d<ad<g d<f+d<gd<ad<b- d<dd<ed<f+d<g d<f+d<gd<ad"
-  "L8 MS <b-d<b-d MLe-<ge-<g MSc<ac<a ML d<fd<f O5 MS b-gb-g"
-  "ML >c#e>c#e MS afaf ML gc#gc# MS fdfd ML e<b-e<b-"
-  "O6 L16ragafaea dac#adaea fa<aa<bac#a dac#adaea faeadaca"
-  "<b-acadg<b-g egdgcg<b-g <ag<b-gcf<af dfcf<b-f<af"
-  "<gf<af<b-e<ge c#e<b-e<ae<ge <fe<ge<ad<fd"
-  "O5 e>ee>ef>df>d b->c#b->c#a>df>d e>ee>ef>df>d"
-  "e>d>c#>db>d>c#b >c#agaegfe f O6 dc#dfdc#<b c#4";
-
 
 void setup()
 {
-  // play note A5
-  buzzer.playNote(NOTE_A(5), 200, 15);
-
   // Initialize the reflectance sensors module
   reflectanceSensors.init();
 
@@ -80,66 +46,55 @@ void setup()
 
   // Turn off LED to indicate we are through with calibration
   digitalWrite(13, LOW);
-  buzzer.play(">g32>>c32");
 
   // Wait for the user button to be pressed and released
   button.waitForButton();
-
-  // Play music and wait for it to finish before we start driving.
-  buzzer.play("L16 cdegreg4");
-  while(buzzer.isPlaying());
 }
 
 void loop()
 {
-  // announch robot name: Mozart
-  // buzzer.playFromProgramSpace(fugue);
   
   unsigned int sensors[6];
-
+  
   // Get the position of the line.  Note that we *must* provide the "sensors"
   // argument to readLine() here, even though we are not interested in the
   // individual sensor readings
   int position = reflectanceSensors.readLine(sensors);
-
+  
   // Our "error" is how far we are away from the center of the line, which
   // corresponds to position 2500.
   int error = position - 2500;
-
+  
   // Get motor speed difference using proportional and derivative PID terms
   // (the integral term is generally not very useful for line following).
   // Here we are using a proportional constant of 1/4 and a derivative
   // constant of 6, which should work decently for many Zumo motor choices.
   // You probably want to use trial and error to tune these constants for
   // your particular Zumo and line course.
+  
   int speedDifference = error / 4 + 6 * (error - lastError);
-
+  
   lastError = error;
-
+  
   // Get individual motor speeds.  The sign of speedDifference
   // determines if the robot turns left or right.
   int m1Speed = MAX_SPEED + speedDifference;
   int m2Speed = MAX_SPEED - speedDifference;
-
+  
   // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
   // Generally speaking, one motor will always be turning at MAX_SPEED
   // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
   // else it will be stationary.  For some applications, you might want to
   // allow the motor speed to go negative so that it can spin in reverse.
   if (m1Speed < 0)
-    m1Speed = 0;
+  m1Speed = 0;
   if (m2Speed < 0)
-    m2Speed = 0;
+  m2Speed = 0;
   if (m1Speed > MAX_SPEED)
-    m1Speed = MAX_SPEED;
+  m1Speed = MAX_SPEED;
   if (m2Speed > MAX_SPEED)
-    m2Speed = MAX_SPEED;
-
-  //m1Speed = MAX_SPEED;
-  //m2Speed = MAX_SPEED;
+  m2Speed = MAX_SPEED;
   
   motors.setSpeeds(m1Speed, m2Speed);
-  
-
   
 }
